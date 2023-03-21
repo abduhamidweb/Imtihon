2b05b16550a1c3473206d6f55732031781fdcab8
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-21 10:31:23 +0500

    error olindi

--- models/sap.categor.js
+++ models/sap.categor.js
@@ -1,15 +1,22 @@
 module.exports = {
   SAP: `SELECT sap_category.* FROM sap_category JOIN categories ON sap_category.catRefid = categories.catId WHERE categories.catName = $1;`,
   SAPALL: 'SELECT * FROM sap_category',
-  SAPID: `SELECT sc.*, v.*, c.comment, u.username
-FROM sap_category sc
-LEFT JOIN videos v ON sc.spaId = v.sap_categoryId
-LEFT JOIN comments c ON v.videoId = c.videoId
-INNER JOIN users u ON c.userId = u.userId
+  SAPID: `
+SELECT *
+FROM videos v
+JOIN sap_category sc ON v.sap_categoryId = sc.spaId
+JOIN categories c ON v.categoriesId = c.catId
 WHERE sc.sapName = $1
 AND v.videoId = $2;
 `,
 }
+//   SELECT sc.*, v.*, c.comment, u.username
+// FROM sap_category sc
+// LEFT JOIN videos v ON sc.spaId = v.sap_categoryId
+// LEFT JOIN comments c ON v.videoId = c.videoId
+// INNER JOIN users u ON c.userId = u.userId
+// WHERE sc.sapName = $1
+// AND v.videoId = $2;
 
 // SELECT v.videoId, v.userId, v.title, v.date, v.categoryId, s.sapName
 // FROM videos v
